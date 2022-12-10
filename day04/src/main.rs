1d4b8dfeb3cd75ca101df1b70bd46ba04fcd1b28
Kevin Ilphrin Pellet <kevinpellet@ilphrin.com>
2022-12-10 18:17:50 +0100

    Handle part two of day 4

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,5 +1,5 @@
 use std::str::FromStr;
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 struct Assignment {
     start: i32,
     end: i32,
@@ -19,14 +19,24 @@ impl FromStr for Assignment {
 }
 
 impl Assignment {
-    fn overlap_with(self, other: &Self) -> bool {
+    fn fully_overlap(self, other: &Self) -> bool {
         if self.start >= other.start && self.end <= other.end {
+            return true;
+        }
+        false
+    }
+
+    fn partially_overlap(self, other: &Self) -> bool {
+        if (other.start..other.end + 1).contains(&self.start)
+            || (other.start..other.end + 1).contains(&self.end)
+        {
             return true;
         }
         false
     }
 }
 
+#[derive(Clone, Copy, Debug)]
 struct Pair {
     first: Assignment,
     second: Assignment,
@@ -47,10 +57,20 @@ impl FromStr for Pair {
 
 impl Pair {
     fn fully_overlap(self) -> i32 {
-        if self.first.overlap_with(&self.second) {
+        if self.first.fully_overlap(&self.second) {
             return 1;
         }
-        if self.second.overlap_with(&self.first) {
+        if self.second.fully_overlap(&self.first) {
+            return 1;
+        }
+        0
+    }
+
+    fn partially_overlap(self) -> i32 {
+        if self.first.partially_overlap(&self.second) {
+            return 1;
+        }
+        if self.second.partially_overlap(&self.first) {
             return 1;
         }
         0
@@ -61,7 +81,7 @@ fn main() -> color_eyre::eyre::Result<()> {
     let sum: i32 = include_str!("input.txt")
         .lines()
         .map(|line| line.parse::<Pair>().unwrap())
-        .map(|assignment| assignment.fully_overlap())
+        .map(|assignment| assignment.partially_overlap())
         .sum();
 
     println!("Number of elves' assignment overlapping: {sum}");

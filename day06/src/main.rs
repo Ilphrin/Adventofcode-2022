12ed297063f845133ec8757f63538f27e8b9baac
Kevin Ilphrin Pellet <kevinpellet@ilphrin.com>
2022-12-10 18:59:02 +0100

    Add day06

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,12 +1,35 @@
-use itertools::Itertools;
+const MARKER_START: usize = 14;
+
+fn find_marker(input: &str) -> usize {
+    input
+        .as_bytes()
+        .windows(MARKER_START)
+        .position(|window| {
+            let mut arr = [false; 256];
+            for &item in window {
+                if arr[item as usize] {
+                    return false;
+                }
+                arr[item as usize] = true;
+            }
+            true
+        })
+        .map(|pos| pos + MARKER_START)
+        .unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::find_marker;
+
+    #[test]
+    fn test_fin_marker() {
+        assert_eq!(7, find_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+    }
+}
 
 fn main() {
-    let line = include_str!("input.txt")
-        .lines()
-        .next()
-        .expect("To be able to read lines");
-    println!("{line}");
-    for (index, char) in line.chars().enumerate() {
-        let slice = line.chars().skip(index).peek_nth(2);
-    }
+    let line = include_str!("input.txt");
+
+    println!("Marker: {:?}", find_marker(line));
 }
